4b1ab5d630e3d59d586989f7f3d108dc92480044
Carl Anderson <nexusrpg@gmail.com>
2013-06-15 21:33:25 -0700

    Inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MicroscopeRpg::Application.config.secret_token = 'b944d822e25bac797d0f7950ba1a26c5c3d5d0436a8ce9fc156c9ecaab5fc19bfcfcaf48f81dc9c1833f149088746322d174486262decd6762986b58d14edc7b'
